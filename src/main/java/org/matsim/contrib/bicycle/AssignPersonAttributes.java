d9dbdda342c3c6995ee53125dd8e799e0076ac8c
clivings <clivings@ivt.baug.ethz.ch>
2019-08-28 19:11:14 +0200

    trying to commit recent changes to my forked branch

---
+++ src/main/java/org/matsim/contrib/bicycle/AssignPersonAttributes.java
@@ -0,0 +1,41 @@
+package org.matsim.contrib.bicycle;
+
+import java.util.Random;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+
+public class AssignPersonAttributes {
+
+	static final String AGE_GROUP_DUMMY_1 = "ageGroupDummy_1";
+	static final String AGE_GROUP_DUMMY_2 = "ageGroupDummy_2";
+	static final String AGE_GROUP_DUMMY_3 = "ageGroupDummy_3";
+	static final String AGE_GROUP_DUMMY_4 = "ageGroupDummy_4";
+	static final String AGE_GROUP_DUMMY_5 = "ageGroupDummy_5";
+	static final String AGE_GROUP_DUMMY_6 = "ageGroupDummy_6";
+
+
+	static public void main (Scenario scenario, Population population){
+		Random random = new Random(1);
+
+		for (Person person : scenario.getPopulation().getPersons().values()) {
+			double r = random.nextDouble();
+
+			if (r<= 0.01) {
+				person.getAttributes().putAttribute(AGE_GROUP_DUMMY_1, 1);
+			} else if (r<= 0.16){
+				person.getAttributes().putAttribute(AGE_GROUP_DUMMY_2, 1);
+			} else if (r<= 0.49) {
+				person.getAttributes().putAttribute(AGE_GROUP_DUMMY_3, 1);
+			} else if (r<= 0.79) {
+				person.getAttributes().putAttribute(AGE_GROUP_DUMMY_4, 1);
+			} else if (r<= 0.94) {
+				person.getAttributes().putAttribute(AGE_GROUP_DUMMY_5, 1);
+			} else {
+				person.getAttributes().putAttribute(AGE_GROUP_DUMMY_6, 1);
+			}
+		}
+
+	}
+}
--- src/main/java/org/matsim/contrib/bicycle/BicycleUtilityUtils.java
+++ src/main/java/org/matsim/contrib/bicycle/BicycleUtilityUtils.java
@@ -6,8 +6,6 @@ import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 
-import bicyclePopulationModifications.AssignPersonAttributes;
-
 public class BicycleUtilityUtils {
 	private final Logger log = Logger.getLogger(BicycleUtilityUtils.class);
 	
